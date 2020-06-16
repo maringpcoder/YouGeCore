e293197b667430bf272f942d19e52cf285945909
marin <123456>
2020-06-16 13:09:22 +0800

    申请退款

--- src/Wx/Common.php
+++ src/Wx/Common.php
@@ -16,7 +16,7 @@ class Common
 {
 
     /**
-     *
+     * 生成分享码
      * md5(md5(code.nonce_str.timestamp).halt)
      * @param GenerateCodeInterface $input
      * @return string
@@ -31,6 +31,26 @@ class Common
             throw new GenerateCodeSignException("code ,nonceStr,halt 不能为空", 2);
         }
         return md5(md5($code . $nonceStr, $halt));
+    }
+
+    /**
+     * 验证是否合法
+     * @param GenerateCodeInterface $input
+     * @param $signInput
+     * @return bool
+     * @throws GenerateCodeSignException
+     */
+    public static function verifyCodeSign(GenerateCodeInterface $input,$signInput)
+    {
+        $code = $input->getCode();
+        $nonceStr = $input->getNonceStr();
+        $halt = $input->getSalt();
+        if (!$code || !$nonceStr || !$halt) {
+            throw new GenerateCodeSignException("code ,nonceStr,halt 不能为空", 2);
+        }
+        $sign = md5(md5($code . $nonceStr, $halt));
+        return $sign === $signInput ? true :false;
+
     }
 
     /**
