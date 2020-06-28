412aa3cf2383192953b342e44c61fff1d82716e5
marin <123456>
2020-06-28 09:55:36 +0800

    引入mongodb

---
+++ src/Mongo/MongodbServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 2020/6/28
+ * Time: 上午9:52
+ */
+
+namespace YouGeCore\Mongo;
+
+
+class MongodbServiceProvider
+{
+
+}
---
+++ src/Mongo/Query/Builder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 2020/6/28
+ * Time: 上午9:53
+ */
+
+namespace YouGeCore\Mongo\Query;
+
+
+class Builder
+{
+
+}
