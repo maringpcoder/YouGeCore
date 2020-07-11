88428a058c2e20705d9a27cd32c1e487a31517a9
marin <123456>
2020-07-12 00:32:59 +0800

    换了一种方式来获取请求信息

--- src/Wx/Pay/WxPayApi.php
+++ src/Wx/Pay/WxPayApi.php
@@ -185,7 +185,7 @@ class WxPayApi
         $xml = $inputObj->ToXml();
         $startTimeStamp = self::getMillisecond();//请求开始时间
         $response = self::postXmlCurl($config, $xml, $url, true, $timeOut);
-        error_log($response,3,'./a.log');
+        var_dump($response);
 
         $result = WxPayResults::Init($config, $response);
         self::reportCostTime($config, $url, $startTimeStamp, $result);//上报请求花费时间
