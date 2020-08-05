677e47302c24d4fe14ff04da6adb73a5e1ad26b1
marin <123456>
2020-08-05 17:01:27 +0800

    setter方法添加

--- src/Common/DataType.php
+++ src/Common/DataType.php
@@ -13,23 +13,32 @@ class DataType
 {
 
     //订单状态  1待支付,2待确认,3待入住, 4已关闭,5,超时未付款自动关闭,  6已完成
-    const Waiting_pay=1;
-    const Waiting_confirmed=2;
-    const Waiting_stay_in=3;
-    const Been_closed=4;
-    const Sys_Closed=5;
-    const Finished=6;
+    const Waiting_pay = 1;
+    const Waiting_confirmed = 2;
+    const Waiting_stay_in = 3;
+    const Been_closed = 4;
+    const Sys_Closed = 5;
+    const Finished = 6;
     //状态描述
-    const ORDER_STATUS_TXT=[
-        self::Waiting_pay=>'待支付',
-        self::Waiting_confirmed=>'待确认',
-        self::Waiting_stay_in=>'待入住',
-        self::Been_closed=>'关闭',
-        self::Sys_Closed=>'自动关闭',
-        self::Finished=>'完成'
+    const ORDER_STATUS_TXT = [
+        self::Waiting_pay => '待支付',
+        self::Waiting_confirmed => '待确认',
+        self::Waiting_stay_in => '待入住',
+        self::Been_closed => '关闭',
+        self::Sys_Closed => '自动关闭',
+        self::Finished => '完成'
     ];
     /** 供应商类型 */
-    const HOTEL_SUP=1;//酒店供应商
-    const LINE_SUP=2;//线路供应商
-    const TICKETS_SUP=3;//门票
+    const HOTEL_SUP = 1;//酒店供应商
+    const LINE_SUP = 2;//线路供应商
+    const TICKETS_SUP = 3;//门票
+    const RENT_CAR = 4;//租车
+
+    /** 所有供应商类型 */
+    const ALL_SUP_TYPE = [
+        self::HOTEL_SUP,
+        self::LINE_SUP,
+        self::TICKETS_SUP,
+        self::RENT_CAR
+    ];
 }
